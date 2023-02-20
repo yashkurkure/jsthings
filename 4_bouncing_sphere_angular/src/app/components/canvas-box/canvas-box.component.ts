99597a4082c622f23a953fdc705812cddc8d93d1
yashkurkure <yash.kurkure1903@gmail.com>
2023-02-19 22:23:50 -0600

    Sphere bouncing and box rotate

--- 4_bouncing_sphere_angular/src/app/components/canvas-box/canvas-box.component.ts
+++ 4_bouncing_sphere_angular/src/app/components/canvas-box/canvas-box.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
 
 @Component({
   selector: 'app-canvas-box',
@@ -7,93 +9,114 @@ import * as THREE from 'three';
   styleUrls: ['./canvas-box.component.css']
 })
 export class CanvasBoxComponent implements OnInit {
+
+
     ngOnInit(): void {
      this.createThreeJsBox();
     }
    
     createThreeJsBox(): void {
+
+        // Setup
         const canvas = document.getElementById('canvas-box');
         const scene = new THREE.Scene();
-
-        const material = new THREE.MeshToonMaterial();
-
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-        scene.add(ambientLight);
-
-        const pointLight = new THREE.PointLight(0xffffff, 0.5);
-        pointLight.position.x = 2;
-        pointLight.position.y = 2;
-        pointLight.position.z = 2;
-        scene.add(pointLight);
-
-        const box = new THREE.Mesh(
-            new THREE.BoxGeometry(1.5, 1.5, 1.5), 
-            material
-         );
-      
-         const torus = new THREE.Mesh(
-            new THREE.TorusGeometry(5, 1.5, 16, 100),
-            material
-         );
-      
-         scene.add(torus, box);
-
-         const canvasSizes = {
-            width: window.innerWidth,
-            height: window.innerHeight,
-           };
-        
-           const camera = new THREE.PerspectiveCamera(
-            75,
-            canvasSizes.width / canvasSizes.height,
-            0.001,
-            1000
-           );
-           camera.position.z = 30;
-           scene.add(camera);
-        
-           if (!canvas) {
+        if (!canvas) {
             return;
-           }
-        
-           const renderer = new THREE.WebGLRenderer({
+            }
+        const renderer = new THREE.WebGLRenderer({
             canvas: canvas,
-           });
-           renderer.setClearColor(0xe232222, 1);
-           renderer.setSize(canvasSizes.width, canvasSizes.height);
+        });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
 
-           window.addEventListener('resize', () => {
-            canvasSizes.width = window.innerWidth;
-            canvasSizes.height = window.innerHeight;
-        
-            camera.aspect = canvasSizes.width / canvasSizes.height;
-            camera.updateProjectionMatrix();
-        
-            renderer.setSize(canvasSizes.width, canvasSizes.height);
+        // Instantiate the OribitControls class.
+        const orbit = new OrbitControls(camera, renderer.domElement);
+
+        // window.addEventListener('resize', () => {
+        // canvasSizes.width = window.innerWidth;
+        // canvasSizes.height = window.innerHeight;
+    
+        // camera.aspect = canvasSizes.width / canvasSizes.height;
+        // camera.updateProjectionMatrix();
+    
+        // renderer.setSize(canvasSizes.width, canvasSizes.height);
+        // renderer.render(scene, camera);
+        // });
+
+        const axesHelper = new THREE.AxesHelper(5);
+        scene.add(axesHelper);
+
+        camera.position.set(-10,30,30);
+        orbit.update();
+
+
+        // Adding a box to the scene
+        // Phase 1: The geometry.
+        const boxGeometry = new THREE.BoxGeometry();
+        // Phase 2: Material of the object.
+        const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+        // Phase 3: Fusion of the geometry and the material. The result if a mesh.
+        const box = new THREE.Mesh(boxGeometry, boxMaterial);
+        scene.add(box);
+
+        // NEW: Adding a plane to the scene
+        const planeGeometry = new THREE.PlaneGeometry(
+            30, // 1st dimension of the plane (x)
+            30 //2nd dimension of th plane (y)
+        );
+        const PlaneMaterial = new THREE.MeshBasicMaterial({
+            color: 0xFFFFFF, // Color of the plane
+            side: THREE.DoubleSide // Apply material to both sides of the plane
+        });
+        const plane = new THREE.Mesh(planeGeometry, PlaneMaterial);
+        scene.add(plane);
+
+        // NEW: rotating a plane
+        plane.rotation.x = -0.5 * Math.PI;
+
+        // NEW: Adding a gird helper
+        const girdHelper = new THREE.GridHelper(
+            30, // size of the grid
+        );
+        scene.add(girdHelper);
+
+        //NEW: Adding a sphere
+        const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+        const sphereMaterial = new THREE.MeshBasicMaterial({
+            color: 0x0000FF,
+            wireframe: true //Display as a wire frame
+        });
+        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+        scene.add(sphere);
+
+        sphere.position.set(-10, 10, 0)
+
+        const clock = new THREE.Clock();
+
+        let step = 0;
+        let speed = 0.01;
+        const animateGeometry = () => {
+            const elapsedTime = clock.getElapsedTime();
+
+            // Update animation objects
+            box.rotation.x += 0.01;
+            box.rotation.y += 0.01;
+
+            step += speed;
+            sphere.position.y = 10*Math.abs(Math.sin(step)) + 4;
+
+            // Render
             renderer.render(scene, camera);
-          });
 
-            const clock = new THREE.Clock();
+            // Call animateGeometry again on the next frame
+            window.requestAnimationFrame(animateGeometry);
+        };
 
-            const animateGeometry = () => {
-                const elapsedTime = clock.getElapsedTime();
-
-                // Update animation objects
-                box.rotation.x = elapsedTime;
-                box.rotation.y = elapsedTime;
-                box.rotation.z = elapsedTime;
-
-                torus.rotation.x = -elapsedTime;
-                torus.rotation.y = -elapsedTime;
-                torus.rotation.z = -elapsedTime;
-
-                // Render
-                renderer.render(scene, camera);
-
-                // Call animateGeometry again on the next frame
-                window.requestAnimationFrame(animateGeometry);
-            };
-
-            animateGeometry();
+        animateGeometry();
         }
    }
